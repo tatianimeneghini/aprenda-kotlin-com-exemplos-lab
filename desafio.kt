fb6d1dc9e7e4b684e831c9175c6d7969d2565b2f
tatianimeneghini <tatiani.meneghini@gmail.com>
2023-10-27 13:23:32 -0300

    feat: atualiza desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,44 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nomeUsuario: String, var email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Inscrito agora: $inscritos em ${conteudos.map { it.nome }}")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1: Usuario = Usuario("Ana", "ana@mail.com")
+    val conteudo1: ConteudoEducacional = ConteudoEducacional(
+        "Abstraindo Formações da DIO Usando Orientação a Objetos com Kotlin",
+        2,
+        Nivel.INTERMEDIARIO
+    )
+    val conteudo2: ConteudoEducacional = ConteudoEducacional(
+        "Conhecendo o Kotlin e Sua Documentação Oficial",
+        1,
+        Nivel.BASICO
+    )
+    val formacao1: Formacao = Formacao("Code Update TQi - Backend com Kotlin e Java",
+        listOf<ConteudoEducacional>(conteudo1, conteudo2)
+    )
+    formacao1.matricular(usuario1)
+
+    val usuario2: Usuario = Usuario("João", "joaojoao@mail.com")
+    val conteudo3: ConteudoEducacional = ConteudoEducacional(
+        "Kubernetes Fundamentals",
+        19,
+        Nivel.DIFICIL
+    )
+    val formacao3: Formacao = Formacao("Code Update TQi - Backend com Kotlin e Java",
+        listOf<ConteudoEducacional>(conteudo3)
+    )
+    formacao3.matricular(usuario2)
 }
